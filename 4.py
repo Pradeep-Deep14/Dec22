a37a72008f871533ba6abfc3514af643d0abd7bc
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-12-22 10:15:58 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,4 @@
+u = ['ab'] * 4  # u is a list containing the string 'ab' repeated 4 times
+v = 'ab'.join(u)  # v is a string created by joining elements of u with 'ab'
+w = v.count('a')  # w is the count of occurrences of 'a' in the string v
+print(v)  # Print the value of w
